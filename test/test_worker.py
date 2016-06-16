6f81db94a40d6a959fcaee8f2fa15e36566724e9
Ning Yang <csyangning@gmail.com>
2016-06-16 15:15:54 -0700

    Add worker and ut.

---
+++ test/test_worker.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from stock_tracer.worker import Worker
+from stock_tracer.library import transaction
+from stock_tracer.model import Stock, Quote, UpdateQuoteAction
+from stock_tracer.test.db import DBUnitTestMixin
+
+class TestWorker(DBUnitTestMixin):
+
+    def setup_method(self, method):
+        """setup_method
+
+        :param method:
+        """
+        super(TestWorker, self).setup_method(method)
+        utc_now = datetime.utcnow()
+        act_time = utc_now - timedelta(hours=1)
+        self.act_time = act_time - timedelta(microseconds=act_time.microsecond)
+
+        with transaction() as tx:
+            tx.add(Stock(exchange="NASDAQ", symbol="AAPL"))
+            scheduled_action = UpdateQuoteAction(action_date=self.act_time, interval_in_second=3600)
+            tx.add(scheduled_action)
+
+    def test_worker_run_scheduled_task(self):
+        """test_worker_run"""
+        worker = Worker()
+        worker.execute_scheduled_task()
+
+        with transaction() as tx:
+            quote = tx.query(Quote).first()
+            action = tx.query(UpdateQuoteAction).first()
+            assert quote
+            assert action.action_date == (self.act_time + timedelta(seconds=3600))
--- worker.py
+++ worker.py
@@ -1,4 +1,31 @@
 from time import sleep
+from datetime import datetime
+from stock_tracer.library import transaction
+from stock_tracer.model import ScheduledAction
 
 class Worker(object):
+    """Worker"""
+    def __init__(self, sleep_internal=600):
+        self.on_line = True
+        self.sleep_internal = sleep_internal
 
+    def run(self):
+        """run"""
+        while self.on_line:
+            self.execute_scheduled_task()
+            sleep(self.sleep_internal)
+
+    def execute_scheduled_task(self):
+        utc_now = datetime.utcnow()
+        with transaction() as tx:
+            actions = tx.query(ScheduledAction).filter(ScheduledAction.action_date < utc_now).all()
+
+        for action in actions:
+            try:
+                action.run()
+            except Exception as e:
+                print(e.message)
+
+if __name__ == "__main__":
+    worker = Worker()
+    worker.run()
