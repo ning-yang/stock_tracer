205068781a1afb91e62e75fef3b087eb360c1fd3
Ning Yang <csyangning@gmail.com>
2016-06-28 23:03:45 -0700

    update docker file and start script.

--- docker/app_run.sh
+++ docker/app_run.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
-echo 'update db...'
-alembic upgrade head
+start_worker() 
+{
+    echo 'start worker...'
+    python scheduler/worker.py 
+}
 
-echo 'start service...'
-python worker.py
+start_service()
+{
+    echo 'start service...'
+    python stock_tracer_service.py
+}
+
+start_ui()
+{
+    echo 'start UI....'
+    export FLASK_APP=stock_tracer.UI.home
+    export FLASK_DEBUG=1
+    flask run --host 0.0.0.0
+}
+
+if [ $1 = "ui" ]; then
+    start_ui
+else
+    echo 'update db...'
+    alembic upgrade head
+
+    if [ $1 = 'service' ]; then
+        start_service
+    else 
+        start_worker
+    fi
+fi
--- docker/docker_deploy.sh
+++ docker/docker_deploy.sh
@@ -1,6 +1,14 @@
 mkdir -p ~/app/log
 mkdir -p ~/app/mysql
 
+# dependency 
 docker run --name mysql-db -v ~/app/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=password -d mysql:5.7
+docker run -d --hostname mq-host --name mq-stock rabbitmq:3
 
-docker run -d --link mysql-dev:mysql -v ~/app:/root/app  ningy/stock_tracer:1.0
+# main app
+docker run -d --link mysql-db:mysql --link mq-stock:rabbit -v ~/app:/root/app --name backend_service  ningy/stock_tracer:1.0 service
+docker run -d --link mysql-db:mysql --link mq-stock:rabbit -v ~/app:/root/app --name worker ningy/stock_tracer:1.0 worker 
+docker run -d --link mysql-db:mysql --link mq-stock:rabbit -p 8080:5000 -v ~/app:/root/app --name frontend_ui ningy/stock_tracer:1.0 ui 
+
+# build
+docker build -t ningy/stock_tracer:1.0 --no-cache=true .
